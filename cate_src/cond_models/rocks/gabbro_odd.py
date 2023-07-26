3b655a20d85560519a04e5a4614fb216441d2314
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2023-07-26 16:55:38 +1000

    ads

---
+++ cate_src/cond_models/rocks/gabbro_odd.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+R_const = 8.3144621
+
+def Dai2015(T, P, water,method):
+
+	#the behaviour of sigma is pretty linear, so I interpolated the p-sigma as what to put on the graph.
+	sigma_through_p = np.array([1.76,1.67,1.52,1.38])
+	p_study = np.array([0.5,1,1.5,2])
+
+	sigma_interp = np.interp(P, p_study, sigma_through_p) #interpolation
+
+	sigma = 10.0**sigma_interp
+	E = 72e3
+	dv = -6.8
+
+	cond = sigma * np.exp(-(E + (P * dv)) / (R_const * T))
+
+	return cond
+
+def Wang2022(T,P,water,method):
+
+	sigma = 92.40 * (1 - (0.23 * P))
+	E = 102e3
+	dv = 6.0
+
+	cond = sigma * np.exp(-(E + (P * dv)) / (R_const * T))
+
+	return cond
+
+	
