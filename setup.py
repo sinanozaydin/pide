b89fd1a40bab302414a410b531a35a75426159cb
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2024-05-09 20:17:39 +1000

    small changes on setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='pide',
-    version='0.1.1',
-    description='A library for petrophysical interpretation.',
-    author='Sinan Ozaydin',
+    version='0.2',
+    description='A library for petrophysical interpretations of geophysical models.',
+    author='Sinan Ozaydin, Lu Li, Utpal Singh, Patrice F. Rey, Maria Constanza Manassero',
     author_email='sinan.ozaydin@protonmail.com',
     url='https://github.com/sinanozaydin/pide',
-    license='MIT',
+    python_requires=">3.8",
+    license='GNU Lesser General Public License v3 (LGPLv3)',
     packages=find_packages(),
     install_requires=[
         'numpy',
@@ -18,4 +22,5 @@ setup(
         'harmonica',
         'pyproj'
     ],
+    keywords = ['petrophysics', 'geodynamic modelling', 'magnetotelluric', 'electrical conductivity', 'seismic velocity']
 )
