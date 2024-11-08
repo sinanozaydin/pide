bc6ca2778bf91d5438c80ffd48b6f23c1f3b6d3e
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2024-11-08 13:22:52 +1100

    Update mt_model_conversion.py

--- pide/mt/mt_model_conversion.py
+++ pide/mt/mt_model_conversion.py
@@ -119,7 +119,7 @@ def convert_2DModel_2_MARE2DEM(file_out, conductivity_array, mesh, boundaries = 
 	print('--------- ' + str(boundaries['bottom']) + '---------')
 	
 	
-def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = None, station_array = None, **kwargs):
+def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, starting_index = None, first_layer_resistivity = None, core_bounds = None, station_array = None, **kwargs):
 
 	#This is a function that converts the constructed 3D geodynamic model into data points
 	#that can be readable by the 3D MT modelling algorithm ModEM.
@@ -140,6 +140,7 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 	"""
 	
 	from scipy.interpolate import griddata
+	import ipdb
 	
 	# core_mesh_size = kwargs.pop('core_mesh_size', mesh[0][0][1][0] - mesh[0][0][0][0])
 	num_horiz_bounds = kwargs.pop('num_horiz_bounds', 8)
@@ -159,7 +160,7 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 				rho[i][j] = conductivity_array[(i*slice_len)+j]
 			except IndexError:
 				raise IndexError('The mesh structure entered does not match the conductivity array. Be sure the entered format mesh = (x_mesh_centers,y_mesh_centers,z_mesh_centers) in tuples are correct.')
-					
+	
 	#determining the indexes
 	start_index_list = []
 	sea_index_list = []
@@ -170,10 +171,18 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 			sea_index_list.append(i)
 	
 	sea_index = sea_index_list[-1]
+
+	#getting rid of the all air layers.
+	if starting_index is None:
+		starting_index = start_index_list[0]
+	else:
+		starting_index = starting_index
+		
+	z_mesh = z_mesh[:starting_index]
 	
 	for i in range(0,len(rho)):
 		if i > sea_index:
-			rho[i][(rho[i] == -999.0)] = 1e-14
+			rho[i][(rho[i] == -999.0)] = -999
 	
 	outs = np.float64(x_mesh[1] - x_mesh[0])
 	xy_out = []
@@ -181,7 +190,7 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 		outs = outs * horiz_bound_incr
 		xy_out.append(outs)
 	xy_out = np.array(xy_out)
-		
+	
 	outs_y = np.float64(y_mesh[1] - y_mesh[0])
 	yx_out = []
 	for i in range(0, num_horiz_bounds):
@@ -194,7 +203,7 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 	for i in range(0, num_vert_bounds):
 		outs_z = outs_z * vert_bound_incr
 		z_out.append(outs_z)
-	outs_z = np.array(outs_z)
+	z_out = np.array(z_out)
 	
 	x_core = np.ones(len(x_mesh)) * np.float64(x_mesh[1] - x_mesh[0])
 	y_core = np.ones(len(y_mesh)) * np.float64(y_mesh[1] - y_mesh[0])
@@ -223,10 +232,12 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 	
 	rho_new = np.array(rho_new)
 	
+	
+	
 	for i in range(0,num_vert_bounds):
 		rho_new = np.insert(rho_new,0,np.ones(len(rho_new[-1])) * np.nan, axis = 0)
 	
-	for i in range(0,len(rho)):
+	for i in range(0,len(z_mesh)):
 		rho_new[i][0] = rho[i][0]
 		rho_new[i][len(x_core) + 2*len(xy_out)] = rho[i][len(x_core)]
 		rho_new[i][-(len(x_core) + 2*len(xy_out))] = rho[i][-len(x_core)]
@@ -288,17 +299,10 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 	
 	#reversing course for writing files
 	rho_write = rho_write[::-1]
+
+	if first_layer_resistivity is not None:
+		rho_write[0][:(len(xi_n) * len(yi_n))] = 1.0 / first_layer_resistivity
 	
-	#finding the index where the layer is all air
-	s_idx = []
-	for i in range(0,len(rho_write)):
-		if np.all(rho_write[i] == rho_write[i][0]) == True:
-			s_idx.append(i)
-			
-	#Getting rid of the layers only contains air
-	rho_write = rho_write[s_idx[-1]+1:]
-	z_out = z_out[s_idx[-1]+1:]
-		
 	rho_write = np.log(1.0 / rho_write) #ModEM rho format with natural logarithm of resistivityrho_
 	
 	lines = ["#  3D MT model written by ModEM in WS format by pide\n"]
@@ -336,15 +340,13 @@ def convert_3DModel_2_ModEM(file_out, conductivity_array, mesh, core_bounds = No
 	
 def create_ModEM_fwd_file(file_out, input_rho, station_location_arrays = None, freq_start=100, freq_end=1e-4, num_freq=120):
 
-	try:
-		file_name = input_rho[input_rho.rfind('/')+1:-3]
-	except:
-		file_name = input_rho[:-3]
-		
+	from pathlib import Path
 	from pide.mt.mt_model_read import read_ModEM_rho
 	from pide.utils.gis_tools import utm_to_lat_lon, lat_lon_to_utm, get_utm_zone_number
 	import decimal
 
+	file_name = Path(input_rho).name[:-4]
+		
 	rho, mesh_centers_x_array, mesh_centers_y_array, z_mesh_center = read_ModEM_rho(rho_file_path=input_rho)
 	
 	#finding the unique grid locations
@@ -441,6 +443,7 @@ def create_ModEM_fwd_file(file_out, input_rho, station_location_arrays = None, f
 def get_station_elevation_ModEM_rho(input_rho, station_location_arrays = None, air_conductivity = 1e-14):
 
 	from pide.mt.mt_model_read import read_ModEM_rho
+	from pide.utils import associate_coordinates
 		
 	if station_location_arrays is None:
 		raise KeyError('Station locations has to be entered to create a file')
