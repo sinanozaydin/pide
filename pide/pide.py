eaca1a48d0f74ca5060e57dde77c6bac3597c9c8
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2025-10-21 16:03:49 +1100

    putting anelasticity functions into a seperate file

--- pide/pide.py
+++ pide/pide.py
@@ -7,7 +7,6 @@ core_path_ext = os.path.join(os.path.dirname(os.path.abspath(__file__)) , 'pide_
 import sys, re, warnings, json, inspect
 import numpy as np
 from scipy.interpolate import interp1d
-from scipy.special import erf
 from santex.isotropy import Isotropy
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -48,6 +47,8 @@ from .pide_src.water_sol.rwd_wds_sol import *
 #importing eos functions
 from .pide_src.eos.fluid_eos import *
 from .pide_src.eos.melt_eos import Holland_Green_Powell_2018_ds633_MeltEOS
+#importin anelasticity functions
+from .pide_src.anelasticity import Q1_Sobolev1996, Q2_Berckhemer1982, YamauchiTakei2016, JacksonFaul2010
 #importing mineral stability functions
 from .pide_src.min_stab.min_stab import *
 #importing utils
@@ -5083,122 +5084,13 @@ class pide(object):
 			v_anelasticity
 		"""
 		if Qmode == 'Q1':
-			#Attenuation model from Sobolev et al. (1996)
-			A = 0.148
-			f = 0.02
-			omega = 2.*np.pi*f
-			a = 0.15
-			H = 500000.
-			V = 0.000020
-			#universal gas constant
-			R = 8.31446
-			E = H + P*1e9*V # Gpa to Pa
-			Q = A*(omega**a)*np.exp((a*E)/R/T)
-			v_anelasticity_b = 1. - 2./Q/np.tan(a*np.pi/2.)/9
-			v_anelasticity_p = 1. - 2./Q/np.tan(a*np.pi/2.)/9
-			v_anelasticity_s = 1. - 1./Q/np.tan(a*np.pi/2.)/2
+			v_anelasticity_b, v_anelasticity_p, v_anelasticity_s = Q1_Sobolev1996(P,T)
 		elif Qmode == 'Q2':
-			#Attenuation model from Berckhemer et al. (1982)
-			A = 0.0002
-			f = 0.02
-			omega = 2.*np.pi*f
-			a = 0.25
-			H = 584000.
-			V = 0.000021
-			#universal gas constant
-			R = 8.31446
-			E = H + P*1e9*V # Gpa to Pa
-			Q = A*(omega**a)*np.exp((a*E)/R/T)
-			v_anelasticity_b = 1. - 2./Q/np.tan(a*np.pi/2.)/9
-			v_anelasticity_p = 1. - 2./Q/np.tan(a*np.pi/2.)/9
-			v_anelasticity_s = 1. - 1./Q/np.tan(a*np.pi/2.)/2
+			v_anelasticity_b, v_anelasticity_p, v_anelasticity_s = Q2_Berckhemer1982(P,T)
 		elif Qmode == 'JF2010':
-			#Attenuation model from Jackson & Faul, 2010
-			A = 816
-			t = 75 # time period (seconds)
-			grain_size = 10 # grain size (mm)
-			a = 0.36
-			E = 293e3 # Activation energy
-			V = 1.2e-5 # Activation volumn
-			#universal gas constant
-			R = 8.314472
-			H = E + P*1e9*V # Gpa to Pa
-			Qs_inv = A*(t/grain_size/1e3*np.exp(-H/R/T))**a
-			Qp_inv = Qs_inv * 4/9
-			v_anelasticity_b = 1. - Qp_inv/np.tan(a*np.pi/2.)/2.
-			v_anelasticity_p = 1. - Qp_inv/np.tan(a*np.pi/2.)/2.
-			v_anelasticity_s = 1. - Qs_inv/np.tan(a*np.pi/2.)/2.
+			v_anelasticity_b, v_anelasticity_p, v_anelasticity_s = JacksonFaul2010(P,T)
 		elif Qmode == 'YT2016':
-			#Attenuation model from Yamauchi & Takei, 2016
-			R=8.3145
-			Ab=0.664
-			alpha=0.38
-			tauP=6.e-5
-			Teta=0.94
-			beta=0.
-			delphi=0.
-			gamma=5.
-			lambdaphi=0
-			TKr=1473.
-			Pr=1.5e9
-			
-			mu0 = 72.45
-			dmudT = -0.01094
-			dmudP = 1.987
-			eta0 = 6.22*10**21
-			E = 462.5e3
-			Va = 7.913e-6
-			dTdz = 2.25
-			sol50 = 1326 # That might change due to composition
-			dep = P * 33
-			Pg=(dep/33.)
-			P=Pg*1.e9 # Gpa to P
-			TK = T # T in K
-			Tsol = sol50 + (dTdz * (dep - 50.0))
-			Tn = TK / (Tsol + 273.0)
-			# ---- Aeta ----
-			Aeta = np.where(
-				Tn < Teta,
-				1.0,
-				np.where(
-					Tn < 1.0,
-					np.exp((-1.0 * ((Tn - Teta) / (Tn - (Tn * Teta)))) * np.log(gamma)),
-					(1.0 / gamma) * np.exp(-delphi),
-				),
-			)
-			# ---- Viscosity ----
-			eta = (
-				eta0
-				* np.exp((E / R) * (1.0 / TK - 1.0 / TKr))
-				* np.exp((Va / R) * (P / TK - Pr / TKr))
-				* Aeta
-			)
-			# ---- Unrelaxed compliance ----
-			Ju = 1.0 / (1.0e9 * (mu0 + (dmudP * Pg) + (dmudT * T)))
-			# ---- Maxwell time and scaled period ----
-			tauM = eta * Ju
-			tau = (3.0 * dep) / 4.2
-			tauS = tau / (2.0 * np.pi * tauM)
-			# ---- Anelastic amplitude (Ap) ----
-			Ap = np.where(
-				Tn < 0.91,
-				0.01,
-				np.where(
-					Tn < 0.96,
-					0.01 + (0.4 * (Tn - 0.91)),
-					np.where(Tn < 1.0, 0.03, 0.03 + beta),
-				),
-			)
-			# ---- Stress parameter (sigmap) ----
-			sigmap = np.where(
-				Tn < 0.92,
-				4.0,
-				np.where(Tn < 1.0, 4.0 + (37.5 * (Tn - 0.92)), 7.0),
-			)
-			factor = (1.+((Ab*(tauS**alpha))/alpha)+((np.sqrt(2.*np.pi)/2.)*Ap*sigmap*(1.-erf((np.log(tauP/tauS))/(np.sqrt(2.)*sigmap)))))
-			v_anelasticity_b = 5/9+4/np.sqrt(factor)/9
-			v_anelasticity_p = 5/9+4/np.sqrt(factor)/9
-			v_anelasticity_s = 1/np.sqrt(factor)
+			v_anelasticity_b, v_anelasticity_p, v_anelasticity_s = YamauchiTakei2016(P,T)
 		elif Qmode == 'none':
 			v_anelasticity_b = 1
 			v_anelasticity_p = 1
@@ -5594,7 +5486,6 @@ class pide(object):
 		self.B_QIF_high = 7.492
 		self.C_QIF_low = 0.044
 		self.C_QIF_high = 0.05
-
 
 		if (mode == 0):
 
