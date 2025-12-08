ad288fe4961ccf9db397691ca6adee9c32d670c4
Sinan Özaydın <63723568+sinanozaydin@users.noreply.github.com>
2025-12-08 15:41:27 +1100

    dobson 2000 magnesiowustite

--- pide/pide_src/cond_models/minerals/other_odd.py
+++ pide/pide_src/cond_models/minerals/other_odd.py
@@ -13,4 +13,9 @@ def Hu2024_Apatite(T, P, water, xFe, param1, fo2 = None, fo2_ref = None, method 
 	cond = 10**sigma * np.exp(-(E + (dv*P)) / (R_const*T))
 	
 	return cond
-	
+
+def Dobson2000_Magnesiowustite_xfe_014(T, P, water, xFe, param1, fo2 = None, fo2_ref = None, method = None, mechanism = None):
+
+	cond =10**(3.13-(0.4484-0.002694*P)*0.4343/(T*8.617e-5))
+
+	return cond
