density calculation methodology and joint step indexing problem fixed.

pide/inversion.py
@@ -1132,7 +1132,11 @@ def _solv_MCMC_column(index, object, depths, moho_depth,
 	proposal_stds = list(proposal_stds)
 	#adding another step for the joint step if triangle_calculation is True.
 	if triangle_calculation == True:
-		proposal_stds = list(proposal_stds) + [np.mean(proposal_stds[-2:])]
+		_idx_fp_std = param_names.index('f_pyx')
+		_idx_fl_std = param_names.index('f_lherz')
+		proposal_stds = list(proposal_stds) + [np.mean([
+			proposal_stds[n_scalars + _idx_fp_std],
+			proposal_stds[n_scalars + _idx_fl_std]])]
 	if param_priors is not None:
 		param_priors = copy.deepcopy(param_priors)
 
@@ -1447,13 +1451,14 @@ def _solv_MCMC_column(index, object, depths, moho_depth,
 
 			object_backup = copy.deepcopy(object)
 			LAB_backup = LAB
+			object.density_fluid_loaded = False
 
 			if rand_dim < n_scalar_total:
 				_lab_temp_proposed = proposed_scalars[1] if invert_lab_temp else lab_temp
 				T_, P_, LAB = _update_geotherm(proposed_scalars[0], _lab_temp_proposed)
 				object.set_temperature(T_)
 				object.set_pressure(P_)
-
+				
 				#if bulk xFe is changed distributing iron among defined minerals.
 				if 'bulk_xfe' in param_names:
 					object.mantle_xfe_distribute(method = 'array')

pide/pide.py
@@ -5155,6 +5155,10 @@ class pide(object):
 			
 		return unique_compositions, fraction_list, idx_unique, id_list_global
 		
+	def _melt_mass_frac_to_vol(self, mass_frac, dens_fluid, dens_solid):
+		
+		return 1.0 / (1 + (((1.0/mass_frac) - 1) * (dens_fluid / dens_solid)))
+		
 	def calculate_seismic_velocities(self, mixing_method = 'HS-Medium', melt_mixing_method = 'HS-Upper', method = 'array', **kwargs):
 	
 		"""
@@ -5270,23 +5274,38 @@ class pide(object):
 				self.v_anelasticity_bulk[index] = v_anelasticity[0]
 				self.v_anelasticity_p[index] = v_anelasticity[1]
 				self.v_anelasticity_s[index] = v_anelasticity[2]
-			
-		if np.mean(self.melt_fluid_mass_frac) != 0.0:
+		
+		if method == 'array':
+			check_seis_melt = (np.mean(self.melt_fluid_mass_frac) != 0.0)
+		elif method == 'index':
+			check_seis_melt = (self.melt_fluid_mass_frac[index] != 0.0)
+		
+		if check_seis_melt == True:
 			
 			if self.density_fluid_loaded == False:
 				
 				if self.seismic_calculation_method == 'modes':
 					self.calculate_density_solid()
-				self.calculate_density_fluid(method = method,sol_idx = index)
 				
-				self.melt_fluid_frac = np.zeros(len(self.melt_fluid_mass_frac))
+				self.calculate_density_fluid(method = method, sol_idx = index)
 				
-				for i in range(0,len(self.melt_fluid_mass_frac)):
+				if (getattr(self, 'melt_fluid_frac', None) is None) or (len(self.melt_fluid_frac) != len(self.T)):
+					self.melt_fluid_frac = np.zeros(len(self.T))
 				
-					if self.melt_fluid_mass_frac[i] != 0.0:
+				if method == 'array':
+
+					for i in range(0,len(self.melt_fluid_mass_frac)):
 						
-						self.melt_fluid_frac[i] = 1.0 / (1 + (((1.0/self.melt_fluid_mass_frac[i]) - 1) * (self.dens_melt_fluid[i] / (self.density_solids[i]))))
-						
+						if self.melt_fluid_mass_frac[i] != 0.0:
+							
+							self.melt_fluid_frac[i] = self._melt_mass_frac_to_vol(mass_frac=self.melt_fluid_mass_frac[i],
+							dens_fluid = self.dens_melt_fluid[i], dens_solid = self.density_solids[i])
+
+				elif method == 'index':
+				
+					self.melt_fluid_frac[index] = self._melt_mass_frac_to_vol(mass_frac=self.melt_fluid_mass_frac[index],
+					dens_fluid = self.dens_melt_fluid[index], dens_solid = self.density_solids[index])
+					
 			if self.seismic_calculation_method == 'gibbs':
 				shear_mod = self.shear_mod_solid.copy()
 				bulk_mod = self.bulk_mod_solid.copy()